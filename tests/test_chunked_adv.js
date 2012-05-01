fd0aa5c96f8fdb95c862d868051037223f4a2297
Darren Liew <ldarren@gmail.com>
2012-05-01 09:33:15 +0000

    added simple comet example

--- tests/test_chunked_adv.js
+++ tests/test_chunked_adv.js
@@ -5,41 +5,56 @@ http = require('http'),
 port = 1337,
 host = null,
 queue = [],
+connection = null,
+output = [],
 id = setInterval(function(){
   var
-  l = queue.length,
-  m;
-  if (l){
-    while(l--){
-      m = queue[l];
-      if (10 === m.index){
-        m.res.end('done');
-        queue.pop();
-        continue;
-      }
-      var
-      data = m.data,
-      c = data.length,
-      p;
-      if (!c) continue;
-      while(c--){
-        p = data[c]
-        m.total += p.param1 + p.param2 + p.param3;
-      }
-      ++m.index;
-      m.res.write(m.total.toString());
-      console.log('send <%s>',m.total.toString());
+  data,load,temp=[];
+  while(queue.length){
+    data = queue.pop();
+    output.push(data.msg);
+    --data.count;
+console.log('count<%s> msg<%s>',data.count, data.msg);
+    if (data.count) temp.push(data);
+  }
+  queue = temp;
+  if (output.length && connection){
+    --connection.count;
+    load = JSON.stringify(output);
+    if (!connection.count){
+      connection.res.end(load);
+      connection = null;
+    }else{
+      connection.res.write(load);
     }
+console.log('send count<%s> load<%s>',connection.count, load);
+    output=[];
   }
 }, 1000),
 server = http.createServer(function(req, res){
+  var
+  query = url.parse(req.url, true).query,
+  role = query['role'],
+  data;
+
   res.writeHead(200, {
     'Content-Type': 'application/octet-stream',
     'Access-Control-Allow-Origin': '*'
   });
-  var data = JSON.parse(url.parse(req.url, true).query['data']);
-  queue.unshift({index:0, total:0, res:res, data:data});
-  console.log(util.inspect(data));
+
+  switch(role){
+    case 'o':
+      data = JSON.parse(query['data']);
+      if (data.length > 0) queue = queue.concat(data);
+      else queue.unshift(data);
+      res.end();
+console.log('received len<%d> data<%s> queue<%s>',data.length,util.inspect(data),util.inspect(queue));
+      break;
+    case 'i':
+      connection = {count:120, res:res};
+console.log('received connection');
+      break;
+  }
 });
 
 server.listen(port, host, function(){
